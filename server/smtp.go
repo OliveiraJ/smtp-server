b82b1096096650dbeea71997312609e933ca65ce
OliveiraJ <jordansilva102@gmail.com>
2021-12-11 21:29:06 -0300

    v2.0 working, pending go back implementation

--- server/smtp.go
+++ server/smtp.go
@@ -5,29 +5,21 @@ import (
 	"net/smtp"
 )
 
-func SendEmail(formIn Input) {
-	//from := "jordansilva102@gmail.com"
-	password := "j81073880"
-
-	// to := []string{
-	// 	"jordansvaoliveira@gmail.com",
-	// 	"JordanOli@protonmail.com",
-	// }
-
+func SendEmail(formIn Input, user User) bool {
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
 
 	message := []byte("Subject: " + formIn.Subject + "\r\n" +
 		"\r\n" + formIn.Message)
 
-	auth := smtp.PlainAuth("", formIn.From, password, smtpHost)
-	if formIn.From != "" {
-		err := smtp.SendMail(smtpHost+":"+smtpPort, auth, formIn.From, formIn.To, message)
+	auth := smtp.PlainAuth("", user.Email, user.Password, smtpHost)
+	if user.Email != "" {
+		err := smtp.SendMail(smtpHost+":"+smtpPort, auth, user.Email, formIn.To, message)
 		if err != nil {
 			fmt.Println(err)
-			return
+			return false
 		}
 	}
-
 	fmt.Println("Email sent succesfully!")
+	return true
 }
